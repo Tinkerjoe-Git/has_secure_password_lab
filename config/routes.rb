f8cf0e9d2364ba13e38dd09803879da6f89e1c66
tinkerjoe-git <nicholas.stagno@gmail.com>
2021-04-20 15:23:25 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  # get 'sessions/new'
+
+  # get 'sessions/create'
+
+  # get 'sessions/destroy'
+
+  # get 'users/new'
+
+  # get 'users/create'
+
+  get '/signup', to: 'users#new', as: :signup
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: :home_page_path
+
+  get '/login', to: 'sessions#new', as: :login
+  post '/sessions', to: 'sessions#create', as: :logging_in
+  delete '/logout', to: 'sessions#destroy', as: :logout
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
